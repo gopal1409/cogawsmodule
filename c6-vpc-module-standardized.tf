bcdf933102144422e900b2c5f3541b3c58e2e5aa
gopal das <gopal1409@gmail.com>
2022-01-18 15:18:49 +0530

    test

--- c6-vpc-module-standardized.tf
+++ c6-vpc-module-standardized.tf
@@ -12,8 +12,8 @@ module "vpc" {
   
   #create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
 
-  enable_nat_gateway = var.vpc_enable_nat_gateway
-  single_nat_gateway = var.vpc_single_nat_gateway
+  #enable_nat_gateway = var.vpc_enable_nat_gateway
+  #single_nat_gateway = var.vpc_single_nat_gateway
 
   enable_dns_hostnames = true
   enable_dns_support   = true
