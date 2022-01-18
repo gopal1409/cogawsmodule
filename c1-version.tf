bcd19f189ecb1c1702b7d20ef1a87b78132b3b11
gopal das <gopal1409@gmail.com>
2022-01-18 15:45:39 +0530

    terraform with backend

--- c1-version.tf
+++ c1-version.tf
@@ -5,6 +5,16 @@ terraform {
       version = "~> 3.0"
     }   
   }
+  backend "s3" {
+    #change the bucket name
+    bucket = "terraform-on-aws-for-ec2-gopal"
+    key = "dev/terraform.tfstate"
+    region = "us-east-1"
+    #change the dyanmodb table name if you have modified it
+    #for state locking
+    dynamodb_table = "dev-project-vpc"
+  }
+}
   
 }
 
--- c9-01-ec2instance-varaible.tf
+++ c9-01-ec2instance-varaible.tf
@@ -9,5 +9,5 @@ variable "instance_keypair" {
 
 variable "private_instance_count" {
   type    = number
-  default = 2
+  default = 1
 }
