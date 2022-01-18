8ae0dd01292e7102a1e3cde93044d042f187913a
gopal das <gopal1409@gmail.com>
2022-01-18 15:09:32 +0530

    db subnet grp disabled

--- c6-vpc-module-standardized.tf
+++ c6-vpc-module-standardized.tf
@@ -7,9 +7,10 @@ module "vpc" {
   azs                                = var.vpc_availability_zone
   public_subnets                     = var.vpc_public_subnets
   private_subnets                    = var.vpc_private_subnets
-  database_subnets                   = var.vpc_database_subnets
-  create_database_subnet_group       = var.vpc_create_database_subnet_group
-  create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
+  #database_subnets                   = var.vpc_database_subnets
+  #create_database_subnet_group       = var.vpc_create_database_subnet_group
+  
+  #create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
 
   enable_nat_gateway = var.vpc_enable_nat_gateway
   single_nat_gateway = var.vpc_single_nat_gateway
